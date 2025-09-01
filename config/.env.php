0f40b722c6fc96b40665906962a0662a10d2804a
bibaowen <bobby.owen@gmail.com>
2025-08-31 16:18:42 -0800

    Delete .env.php

--- config/.env.php
+++
@@ -1,9 +0,0 @@
-<?php
-// file: config/env.php
-define("MYSQL_HOST", "https://phpstack-1515092-5822114.cloudwaysapps.com/");
-define("MYSQL_DATABASE", "knqdjvhhkw");
-define("MYSQL_USER", "knqdjvhhkw");
-define("MYSQL_PASSWORD", "QJC5ag9sWS");
-define("MYSQL_PORT", "3306");
-
-define("API_BASE", "https://sample-stack.onrender.com");
